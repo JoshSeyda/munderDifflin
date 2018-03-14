f1d445f9f62d3b8bb03e2a0fdd06a77cacc3bab1
Josh Seyda <josh.seyda@gmail.com>
2018-03-14 19:07:36 -0400

    fixed mobile responsive

--- main.js
+++ main.js
@@ -26,33 +26,6 @@
 // 		}
 // });
 
-// $(document).ready(function(){
-//   $('.toggle').click(function(){
-//     $('#nav').toggleClass('open');
-//     $('.container').toggleClass('menu-open');
-//   });
-// });
-
-
-// var acc = document.getElementsByClassName("supplement");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//     acc[i].addEventListener("click", function() {
-//         /* Toggle between adding and removing the "active" class,
-//         to highlight the button that controls the panel */
-//         this.classList.toggle("active");
-
-//         /* Toggle between hiding and showing the active panel */
-//         var panel = this.nextElementSibling;
-//         if (panel.style.display === "block") {
-//             panel.style.display = "none";
-//         } else {
-//             panel.style.display = "block";
-//         }
-//     });
-// }
-
 
 var collapse = document.getElementsByClassName("collapsible");
 var i;
